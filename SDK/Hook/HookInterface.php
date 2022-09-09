06b207fc9ba1276206c84b4fdbce55a0ff3633f7
Vyacheslav Ovchinnikov <vovchinnikov@plesk.com>
2022-09-09 12:29:45 +0300

    Update stubs with 18.0.47 SDK

---
+++ SDK/Hook/HookInterface.php
@@ -0,0 +1,11 @@
+<?php
+// Copyright 1999-2022. Plesk International GmbH. All rights reserved.
+
+namespace Plesk\SDK\Hook;
+
+/**
+ * @since 18.0.47
+ */
+interface HookInterface extends \pm_Hook_Interface
+{
+}
---
+++ SDK/Webmail.php
@@ -0,0 +1,75 @@
+<?php
+// Copyright 1999-2022. Plesk International GmbH. All rights reserved.
+
+namespace Plesk\SDK;
+
+/**
+ * @since 18.0.47
+ */
+final class Webmail
+{
+    private string $name;
+    private string $displayName;
+    private string $loginLinkTemplate = '{{ protocol }}://webmail.{{ domainName }}/';
+    private \Closure $isVisibleCallback;
+
+    /**
+     * @param string $name Webmail name that will be used as an identifier of this webmail. Must be unique within the extension.
+     * @param string $displayName Name that will be displayed in Plesk UI.
+     */
+    public function __construct(string $name, string $displayName)
+    {
+        $this->name = $name;
+        $this->displayName = $displayName;
+        $this->isVisibleCallback = \Closure::fromCallable(fn (): bool => true);
+    }
+
+    /**
+     * Mustache template that will generate the login link to the webmail in Plesk UI.
+     *
+     * For example: {{ protocol }}://webmail.{{ domainName }}/index.php?login={{ fullMailName }}
+     * Available context:
+     * * domainName: string, domain name of the selected domain
+     * * mailName: string, local part of the email
+     * * fullMailName: string, full email address, urlencoded (e.g. user%40example.com)
+     * * protocol: string, either "http", or "https"
+     *
+     * @param string $loginLinkTemplate
+     * @return $this
+     *
+     * @since 18.0.47
+     */
+    public function setLoginLinkTemplate(string $loginLinkTemplate): self
+    {
+        $this->loginLinkTemplate = $loginLinkTemplate;
+        return $this;
+    }
+
+    /**
+     * A callback function that decides whether webmail should be visible for a domain or not.
+     *
+     * @param callable $callback Callback that takes \pm_Domain as an argument and returns a boolean value.
+     * @return $this
+     *
+     * @since 18.0.47
+     */
+    public function setIsVisibleForDomainCallback(callable $callback): self
+    {
+        $this->isVisibleCallback = \Closure::fromCallable($callback);
+        return $this;
+    }
+
+    /**
+     * @ignore
+     * @return array{name: string, displayName: string, loginLinkTemplate: string, isVisibleCallback: \Closure}
+     */
+    public function getData(): array
+    {
+        return [
+            'name' => $this->name,
+            'displayName' => $this->displayName,
+            'loginLinkTemplate' => $this->loginLinkTemplate,
+            'isVisibleCallback' => $this->isVisibleCallback,
+        ];
+    }
+}

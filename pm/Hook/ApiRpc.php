deb1ea665881c7c5780093a12993d9b14dcf7bea
Aleksei Fedorov <aleksei.fedorov@webpros.com>
2025-02-11 11:43:10 +0200

    Update copyrights

--- pm/Hook/ApiRpc.php
+++ pm/Hook/ApiRpc.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2024. WebPros International GmbH. All rights reserved.
+// Copyright 1999-2025. WebPros International GmbH. All rights reserved.
 /**
  * Hook for processing remote API-RPC requests
  *
--- pm/Log.php
+++ pm/Log.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2024. WebPros International GmbH. All rights reserved.
+// Copyright 1999-2025. WebPros International GmbH. All rights reserved.
 /**
  * Class pm_Log
  * Intended to log various data.
